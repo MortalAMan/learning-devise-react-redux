1d4065a876d843d158661e43c634172c9225b54c
Jake Sorce <jakesorce@gmail.com>
2017-06-28 12:07:49 -0600

    no match and register components

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class NoMatch extends Component {
   render() {
     return(
-      <Header as='h1' textAlign='center'>No Match Component</Header>
+      <Header as='h1' textAlign='center'>
+        Page Not Found
+        <Link to='/'> Home</Link>
+      </Header>
     );
   }
 }
